b311c6f4186281242376a840eba50342c828f5b6
biomancer <biomancer@gmail.com>
2015-04-03 19:33:56 +0300

    Respect slack_stage variable

--- lib/mina/slack/defaults.rb
+++ lib/mina/slack/defaults.rb
@@ -2,7 +2,7 @@
 set_default :slack_url,       -> { ENV['SLACK_URL'] }
 set_default :slack_room,        -> { ENV['SLACK_ROOM'] }
 # Optional
-set_default :slack_stage,       -> { ENV['SLACK_STAGE'] || fetch(:rails_env, 'production') }
+set_default :slack_stage,       -> { ENV['SLACK_STAGE'] || ENV['to'] || fetch(:rails_env, 'production') }
 set_default :slack_application, -> { ENV['SLACK_APPLICATION'] || application }
 set_default :slack_username,    -> { ENV['SLACK_USERNAME'] || 'deploybot' }
 set_default :slack_emoji,       -> { ENV['SLACK_EMOJI'] || ':cloud:' }
