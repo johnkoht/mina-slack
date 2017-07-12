acd547a0cd9ea16fca63b8c4b530cead7fab3832
Karel Leinhaupl <karel.leinhaupl@eman.cz>
2017-07-12 10:32:30 +0200

    Fix mina-slack defaults

--- lib/mina/slack/defaults.rb
+++ lib/mina/slack/defaults.rb
@@ -8,6 +8,6 @@ set :slack_username,    -> { ENV['SLACK_USERNAME'] || 'deploybot' }
 set :slack_emoji,       -> { ENV['SLACK_EMOJI'] || ':cloud:' }
 # Git
 set :deployer,          -> { ENV['GIT_AUTHOR_NAME'] || %x[git config user.name].chomp }
-set :deployed_revision, -> { ENV['GIT_COMMIT'] || %x[git rev-parse #{branch}].strip }
+set :deployed_revision, -> { ENV['GIT_COMMIT'] || %x[git rev-parse #{fetch(:branch)}].strip }
 
-set :changes,           -> { ENV['CHANGES'] || %x[git rev-list --pretty=format:'%an (%ad) - %s%n' --date=format:'%d-%m-%Y' --abbrev-commit  #{last_revision}..#{deployed_revision}]}
+set :changes,           -> { ENV['CHANGES'] || %x[git rev-list --pretty=format:'%an (%ad) - %s%n' --date=format:'%d-%m-%Y' --abbrev-commit  #{fetch(:last_revision)}..#{fetch(:deployed_revision)}]}
