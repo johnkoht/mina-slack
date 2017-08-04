41a536e42be3ba933ac1463d7a08ceebf4864bc8
Karel Leinhaupl <karel.leinhaupl@eman.cz>
2017-08-04 11:33:03 +0200

    Fix fetch Enviroment

--- lib/mina/slack/defaults.rb
+++ lib/mina/slack/defaults.rb
@@ -2,7 +2,7 @@
 set :slack_url,       -> { ENV['SLACK_URL'] }
 set :slack_room,        -> { ENV['SLACK_ROOM'] }
 # Optional
-set :slack_stage,       -> { ENV['SLACK_STAGE'] || ENV['to'] || fetch(:rails_env) }
+set :slack_stage,       -> { ENV['SLACK_STAGE'] || ENV['TO'] || ENV['to'] || fetch(:rails_env) }
 set :slack_application, -> { ENV['SLACK_APPLICATION'] || application }
 set :slack_username,    -> { ENV['SLACK_USERNAME'] || 'deploybot' }
 set :slack_emoji,       -> { ENV['SLACK_EMOJI'] || ':cloud:' }
