d5984e5b92680dddadf62bdb0f7912b283f195f6
Karel Leinhaupl <karel.leinhaupl@eman.cz>
2017-09-14 09:47:22 +0200

    Update git commands

--- lib/mina/slack/tasks.rb
+++ lib/mina/slack/tasks.rb
@@ -4,16 +4,16 @@ namespace :slack do
     if (url = fetch(:slack_url)) && (room = fetch(:slack_room))
       if set?(:user)
         Net::SSH.start(fetch(:domain), fetch(:user)) do |ssh|
-          set(:last_revision, ssh.exec!("cd #{fetch(:deploy_to)}/scm; git log #{fetch(:branch)} -n 1 --pretty=format:'%H'"))
+          set(:last_revision, ssh.exec!("cd #{fetch(:deploy_to)}/scm; git log -n 1 --pretty=format:'%H' #{fetch(:branch)} --"))
         end
       else
         login_data = fetch(:domain).split('@')
         Net::SSH.start(login_data[1], login_data[0]) do |ssh|
-          set(:last_revision, ssh.exec!("cd #{fetch(:deploy_to)}/scm; git log #{fetch(:branch)} -n 1 --pretty=format:'%H'"))
+          set(:last_revision, ssh.exec!("cd #{fetch(:deploy_to)}/scm; git log -n 1 --pretty=format:'%H' #{fetch(:branch)} --"))
         end
       end
 
-      set(:last_commit, `git log #{fetch(:branch)} -n 1 --pretty=format:"%H"`)
+      set(:last_commit, `git log -n 1 --pretty=format:"%H" #{fetch(:branch)} --`)
       changes
       send_slack_message(slack_deploy_message, url)
     else
