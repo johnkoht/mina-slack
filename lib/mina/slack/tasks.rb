ef9f82370facb602c3e69cdbca407836990c2475
eMan s.r.o <distribution@eman.cz>
2018-04-25 10:40:03 +0200

    Fix domain settings without user

--- lib/mina/slack/tasks.rb
+++ lib/mina/slack/tasks.rb
@@ -2,15 +2,15 @@
 namespace :slack do
   task :post_info do
     if (url = fetch(:slack_url)) && (room = fetch(:slack_room))
-      if set?(:user)
-        Net::SSH.start(fetch(:domain), fetch(:user)) do |ssh|
-          set(:last_revision, ssh.exec!("cd #{fetch(:deploy_to)}/scm; git log -n 1 --pretty=format:'%H' #{fetch(:branch)} --"))
-        end
+      login_data = if (user = fetch(:user))
+        [ fetch(:domain), user ]
       else
-        login_data = fetch(:domain).split('@')
-        Net::SSH.start(login_data[1], login_data[0]) do |ssh|
-          set(:last_revision, ssh.exec!("cd #{fetch(:deploy_to)}/scm; git log -n 1 --pretty=format:'%H' #{fetch(:branch)} --"))
-        end
+        # "bob@127.5.1.2".split('@')
+        fetch(:domain).split('@').reverse
+      end
+      
+      Net::SSH.start(login_data[0], login_data[1]) do |ssh|
+        set(:last_revision, ssh.exec!("cd #{fetch(:deploy_to)}/scm; git log -n 1 --pretty=format:'%H' #{fetch(:branch)} --"))
       end
 
       set(:last_commit, `git log -n 1 --pretty=format:"%H" origin/#{fetch(:branch)} --`)
