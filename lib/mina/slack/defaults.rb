28d0af5f0c73a89e0455fa1615da0cf46da8d7cb
Laurent Vallar <val@zbla.net>
2018-06-21 17:39:12 +0200

    update set_default calls mina version >= 1.X
    Fix #4
    
    Signed-off-by: Laurent Vallar <val@zbla.net>

--- lib/mina/slack/defaults.rb
+++ lib/mina/slack/defaults.rb
@@ -1,11 +1,11 @@
 # Required
-set_default :slack_url,       -> { ENV['SLACK_URL'] }
-set_default :slack_room,        -> { ENV['SLACK_ROOM'] }
+set :slack_url,       -> { ENV['SLACK_URL'] }
+set :slack_room,        -> { ENV['SLACK_ROOM'] }
 # Optional
-set_default :slack_stage,       -> { ENV['SLACK_STAGE'] || ENV['to'] || fetch(:rails_env, 'production') }
-set_default :slack_application, -> { ENV['SLACK_APPLICATION'] || application }
-set_default :slack_username,    -> { ENV['SLACK_USERNAME'] || 'deploybot' }
-set_default :slack_emoji,       -> { ENV['SLACK_EMOJI'] || ':cloud:' }
+set :slack_stage,       -> { ENV['SLACK_STAGE'] || ENV['to'] || fetch(:rails_env, 'production') }
+set :slack_application, -> { ENV['SLACK_APPLICATION'] || application }
+set :slack_username,    -> { ENV['SLACK_USERNAME'] || 'deploybot' }
+set :slack_emoji,       -> { ENV['SLACK_EMOJI'] || ':cloud:' }
 # Git
-set_default :deployer,          -> { ENV['GIT_AUTHOR_NAME'] || %x[git config user.name].chomp }
-set_default :deployed_revision, -> { ENV['GIT_COMMIT'] || %x[git rev-parse #{branch}].strip }
+set :deployer,          -> { ENV['GIT_AUTHOR_NAME'] || %x[git config user.name].chomp }
+set :deployed_revision, -> { ENV['GIT_COMMIT'] || %x[git rev-parse #{branch}].strip }
